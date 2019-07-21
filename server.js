5363332930179120f211460d9812d00905be7a66
unknown <huandrew93@gmail.com>
2019-07-20 19:23:22 -0500

    changed html file name

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ let express = require('express');
 let serveStatic = require('serve-static');
 app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public/'));
 
 let port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
 
 app.get('/', function (req, res) {
-  res.redirect('/Baccarat.html');
+  res.redirect('/index.html');
 })
